c4938266a25a2ed58cdac87bdafc705573174fb4
Towa Quimbayo <towacurtis@hotmail.com>
2025-01-18 15:33:42 -0800

    added inter font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { Inter: ["Inter", "sans-serif"] },
+    },
   },
   plugins: [],
 };
