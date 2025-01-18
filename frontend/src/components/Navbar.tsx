8097e36795e25f2f4dcb3767acb205d5e9261404
Towa Quimbayo <towacurtis@hotmail.com>
2025-01-18 15:34:29 -0800

    Completed initial bottom nav styling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,56 @@
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const isPageActive = (path: string) => {
+    if (window.location.pathname === path) {
+      return "text-[#3431c7] font-medium transition duration-300 ease-in-out underline underline-offset-8";
+    } else {
+      return "text-gray-500 hover:text-[#3431c7] transition duration-300 ease-in-out";
+    }
+  };
+
   return (
-    <nav>
-      <h1>Omni</h1>
+    <nav className="absolute bottom-6 left-16 right-16">
+      <div className="w-full bg-[#f8f8f8] shadow-sm rounded-full flex justify-between items-center p-6">
+        <h3
+          className="text-xl font-semibold cursor-pointer"
+          onClick={() => navigate("/rooms")}
+        >
+          Omni
+        </h3>
+        <div className="flex gap-12">
+          <Link
+            className={isPageActive("/rooms")}
+            to="/rooms"
+            aria-current="page"
+          >
+            Rooms
+          </Link>
+          <Link className={isPageActive("/devices")} to="/devices">
+            Devices
+          </Link>
+          <Link className={isPageActive("/automations")} to="/automations">
+            Automations
+          </Link>
+          {/* @TODO: Fix logout */}
+          <button className="text-gray-500" onClick={() => alert("Logout")}>
+            Logout
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-2 px-4 py-2 bg-white rounded-full">
+          <img
+            src="../assets/profile.jpg"
+            alt="Profile Image"
+            className="w-10 h-10 rounded-full"
+          />
+          <div className="flex flex-col">
+            <span className="text-sm text-gray-500">@elmoworld</span>
+            <span className="font-semibold">Elmo World</span>
+          </div>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
