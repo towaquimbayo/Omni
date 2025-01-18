3c2f57dbe9ceb26b6022c7c92c395e1db156a0ee
Towa Quimbayo <towacurtis@hotmail.com>
2025-01-18 12:36:36 -0800

    added auth page routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Signup />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
