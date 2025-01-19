11011a21e40975dca4a3b7ef49c6077f37024bc6
Mangat Toor <immangat@gmail.com>
2025-01-19 01:04:28 -0800

    refactor(config): add debug method

--- backend/config.py
+++ backend/config.py
@@ -10,3 +10,13 @@ class Config:
     DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
     HOME_ASSISTANT_ID = os.getenv('HOME_ASSISTANT_ID', '')
     HOME_ASSISTANT_URL = os.getenv('HOME_ASSISTANT_URL', '')
+
+    def __str__(self):
+        return (f"Config(\n"
+                f"  KEYWORD_FILE_PATH: {self.KEYWORD_FILE_PATH},\n"
+                f"  PICOVOICE_ACCESS_KEY: {self.PICOVOICE_ACCESS_KEY},\n"
+                f"  AUDIO_DEVICE_INDEX: {self.AUDIO_DEVICE_INDEX},\n"
+                f"  DEBUG: {self.DEBUG},\n"
+                f"  HOME_ASSISTANT_ID: {self.HOME_ASSISTANT_ID},\n"
+                f"  HOME_ASSISTANT_URL: {self.HOME_ASSISTANT_URL}\n"
+                f")")
