63eb80e083342936fc6d7040d57c8c6bfd79b717
Towa Quimbayo <towacurtis@hotmail.com>
2025-01-18 18:18:33 -0800

    added automation and devices

---
+++ frontend/src/pages/Automations.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Automations() {
+  return (
+    <Layout title="Automations">
+      <h1>Automations</h1>
+    </Layout>
+  );
+}
---
+++ frontend/src/pages/Devices.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Devices() {
+  return (
+    <Layout title="Devices">
+      <h1>Devices</h1>
+    </Layout>
+  );
+}
